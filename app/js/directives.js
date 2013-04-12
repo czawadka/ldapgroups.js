da2860885bf3e4a7bb63cebca687268b17de404f
czawadka <czawadka@gmail.com>
2013-04-13 00:40:35 +0200

    bootstrap tabs initilized by activeLinkClass directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,29 @@
 
 /* Directives */
 
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+var module = angular.module('myApp.directives', []);
+module.directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
+    };
+  }]);
+module.directive('activeLinkClass', ['$location', function($location) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+            var clazz = attrs.activeLinkClass;
+            //var path = attrs.href;
+            var path = (element.tagName=='A' ? element : element.find('A')).attr('href');
+            path = path.substring(1); //hack because path does bot return including hashbang
+            scope.location = $location;
+            scope.$watch('location.path()', function(newPath) {
+                if (path === newPath) {
+                    element.addClass(clazz);
+                } else {
+                    element.removeClass(clazz);
+                }
+            });
+        }
+
     };
   }]);
