3291f400f640669b83f30968984beab4467cc4f3
czawadka <czawadka@gmail.com>
2013-04-17 23:19:34 +0200

    added Group service with sample data

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,11 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+    .value('version', '0.1')
+    .factory('Group', function($resource){
+        return $resource('rest/groups/:groupName.json', {}, {
+            query: {method:'GET', params:{groupName:'groups'}, isArray:true}
+        });
+    })
+;
