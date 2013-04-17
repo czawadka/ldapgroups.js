97091c1de3f91b71a4bcbd222f64abb97a0efcd5
czawadka <czawadka@gmail.com>
2013-04-17 23:23:25 +0200

    added /groups controller&view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,11 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('myApp.controllers', ['myApp.services'])
+    .controller('GroupsCtrl', ['$scope', 'Group', function($scope, Group) {
+        $scope.groups = Group.query();
+    }])
+    .controller('GroupCtrl', ['$scope', '$routeParams', 'Group', function($scope, $routeParams, Group) {
+        $scope.group = Group.get({groupName: $routeParams.groupName});
+    }])
+;
