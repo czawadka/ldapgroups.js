9f4eb6cbd53851d2654343858fa70d1a0ee980fe
czawadka <czawadka@gmail.com>
2013-04-20 22:11:01 +0200

    added escape url filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,13 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('myApp.filters', [])
+    .filter('interpolate', ['version', function(version) {
+        return function(text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }])
+    .filter('escape', function() {
+        return window.escape;
+    })
+;
