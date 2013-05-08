e306205967d9aad135e074fd01c9de5d93fb9336
czawadka <Cezary.Zawadka@ydp.eu>
2013-05-08 12:13:54 +0200

    added ability to configure Group service

--- app/js/services.js
+++ app/js/services.js
@@ -5,11 +5,13 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', ['ngResource'])
+var myAppServices = angular.module('myApp.services', ['ngResource'])
     .value('version', '0.1')
-    .factory('Group', function($resource){
-        return $resource('rest/groups/:groupName.json', {}, {
-            query: {method:'GET', params:{groupName:'groups'}, isArray:true}
+    .value('groupUrl', 'rest/groups/:groupName')
+    .value('groupListParams', {groupName:null})
+    .factory('Group', function($resource, groupUrl, groupListParams){
+        return $resource(groupUrl, {}, {
+            query: {method:'GET', params:groupListParams, isArray:true}
         });
     })
 ;
