602a6b5bad1f10aa68848d460d33e490b227a172
czawadka <czawadka@gmail.com>
2013-05-16 00:30:55 +0200

    added ability to create group

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,14 @@ angular.module('myApp.controllers', ['myApp.services'])
     .controller('GroupsCtrl', ['$scope', 'Group', 'MainBreadcrumbs', function($scope, Group, MainBreadcrumbs) {
         $scope.groups = Group.query();
         MainBreadcrumbs.groups();
+
+        $scope.addGroup = function() {
+            var newGroup = new Group({name: $scope.groupName});
+            newGroup.$save();
+            $scope.groups.push(newGroup);
+
+            $scope.groupName = "";
+        }
     }])
     .controller('GroupCtrl', ['$scope', '$routeParams', 'Group', 'MainBreadcrumbs', function($scope, $routeParams, Group, MainBreadcrumbs) {
         $scope.group = Group.get({groupName: $routeParams.groupName});
