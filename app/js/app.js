d5896a3812b354277d61bf1a6c7e34321d114d63
czawadka <Cezary.Zawadka@ydp.eu>
2013-05-19 22:52:37 +0200

    introduced centralized path configuration

--- app/js/app.js
+++ app/js/app.js
@@ -3,28 +3,32 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('ldapgroups', ['ldapgroupsFilters', 'ldapgroupsServices', 'ldapgroupsDirectives', 'ldapgroupsControllers', 'breadcrumbs'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/groups', {templateUrl: 'partials/group-list.html', controller: 'GroupsCtrl'});
-        $routeProvider.when('/groups/:groupName', {templateUrl: 'partials/group-details.html', controller: 'GroupCtrl'});
-        $routeProvider.when('', {redirectTo: '/groups'});
+    .constant('paths', {"groups": '/groups', "notFound": '/notFound'})
+    .config(['$routeProvider', 'paths', function ($routeProvider, paths) {
+        $routeProvider.when(paths.groups, {templateUrl: 'partials/group-list.html', controller: 'GroupsCtrl'});
+        $routeProvider.when(paths.groups+'/:groupName', {templateUrl: 'partials/group-details.html', controller: 'GroupCtrl'});
+        $routeProvider.when('', {redirectTo: paths.groups});
         $routeProvider.otherwise({templateUrl: 'partials/error-notfound.html', controller: 'NotFoundCtrl'});
     }])
-    .factory('MainBreadcrumbs', ['Breadcrumbs', function(Breadcrumbs) {
+    .run(['$rootScope', 'paths', function ($rootScope, paths) {
+        $rootScope.paths = paths;
+    }])
+    .factory('MainBreadcrumbs', ['Breadcrumbs', 'paths', function(Breadcrumbs, paths) {
         var base = Breadcrumbs.get();
         var extra = {
             groups: function() {
                 return this.beginUpdate()
-                    .clear().push('#/group', 'Groups')
+                    .clear().push('#'+paths.groups, 'Groups')
                     .endUpdate();
             },
             group: function(groupName) {
                 return this.beginUpdate()
-                    .groups().push('#/group/'+groupName, groupName)
+                    .groups().push('#'+paths.groups+'/'+groupName, groupName)
                     .endUpdate();
             },
             notFound: function() {
                 return this.beginUpdate()
-                    .clear().push('#/notFound', "Not found")
+                    .clear().push('#'+paths.notFound, "Not found")
                     .endUpdate();
             }};
         return angular.extend(extra, base);
