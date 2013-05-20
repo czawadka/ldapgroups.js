3354f487d1e9b79da65cfd031021d8605d880f7b
czawadka <Cezary.Zawadka@ydp.eu>
2013-05-20 17:36:22 +0200

    added ability to remove group

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,17 +3,33 @@
 /* Controllers */
 
 angular.module('ldapgroupsControllers', ['ldapgroupsServices'])
-    .controller('GroupsCtrl', ['$scope', 'Group', 'MainBreadcrumbs', function($scope, Group, MainBreadcrumbs) {
+    .controller('GroupsCtrl', ['$scope', 'Group', 'MainBreadcrumbs', '$log', function($scope, Group, MainBreadcrumbs, $log) {
         $scope.groups = Group.query();
         MainBreadcrumbs.groups();
 
         $scope.addGroup = function() {
-            var newGroup = new Group({name: $scope.groupName});
-            newGroup.$save();
-            $scope.groups.push(newGroup);
+            Group.create({name: $scope.groupName},
+                function(newGroup) {
+                    $scope.groups.push(newGroup);
+                    $scope.groupName = "";
+                },
+                function(response) {
+                    $log.log( response, "error adding group" );
+                });
+        };
 
-            $scope.groupName = "";
-        }
+        $scope.removeGroup = function(group) {
+            group.$delete(
+                function() {
+                    var idx = $scope.groups.indexOf(group);
+                    if (idx>=0) {
+                        $scope.groups.splice(idx, 1);
+                    }
+                },
+                function(response) {
+                    $log.log( "error removing group: "+response );
+                });
+        };
     }])
     .controller('GroupCtrl', ['$scope', '$routeParams', 'Group', 'MainBreadcrumbs', function($scope, $routeParams, Group, MainBreadcrumbs) {
         $scope.group = Group.get({groupName: $routeParams.groupName});
--- app/js/services.js
+++ app/js/services.js
@@ -7,11 +7,12 @@
 // In this case it is a simple value service.
 var ldapgroupsServices = angular.module('ldapgroupsServices', ['ngResource'])
     .value('version', '0.1')
-    .value('groupUrl', 'api/groups/:groupName')
-    .value('groupListParams', {groupName:''})
-    .factory('Group', function($resource, groupUrl, groupListParams){
-        return $resource(groupUrl, {}, {
-            query: {method:'GET', params:groupListParams, isArray:true}
+    .value('groupApiUrl', 'api/groups/:groupName')
+    .factory('Group', ['$resource', 'groupApiUrl', function($resource, groupApiUrl){
+        return $resource(groupApiUrl, {groupName: '@name'}, {
+            query: {method:'GET', params: {groupName: ''}, isArray:true},
+            create: {method:'POST', params: {groupName: ''}},
+            save: {method:'PUT'}
         });
-    })
+    }])
 ;
